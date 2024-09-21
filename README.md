927125fbddfa23d6aa077e0553f401a98d4055fb
phitoduck <eric.riddoch@gmail.com>
2024-09-21 02:54:56 -0600

    added a docker compose for a 'remote deployment' but locally

--- README.md
+++ README.md
@@ -1,18 +1,62 @@
+## Usage
 
+### Development with remote deployment
 
+```bash
+# re-deploy the EC2 instance stack
+cd iac
+make install
+cdk deploy --app 'python app.py' --region us-west-2 --profile zenml
+
+# auth with the deployed zenml server
+zenml connect --server-url http://<ec2 ip>:8080
+
+# create our zenml components and stack
+./setup-zenml.sh
+```
+
+### Clean it up so no one can hack us
+
+```bash
+cdk destroy --app 'python app.py' --region us-west-2 --profile zenml
+```
+
+### Local development
+
+```bash
+docker-compose up
+zenml connect --server-url http://localhost:8080
+./setup-zenml.sh
+```
 
 ## ZenML OSS vs Metaflow OSS
 
 | Feature | ZenML OSS | Metaflow OSS |
 | --- | - | - |
-| Preview Artifacts in the UI | ❌ | ❌ |
-| Arbitrary visualizations in UI |  | ✅ (metaflow cards) |
-| Trigger DAGs from the UI | ❌ | ❌ (seems like not based on [this](https://github.com/Netflix/metaflow-ui/blob/master/README.mds)) |
+| List/Preview Artifacts in the UI | ❌ | ❌ |
+| Arbitrary visualizations in UI | ❌ | ✅ (metaflow cards) |
+| Trigger DAGs from the UI | ❌ | ❌ (seems like no based on [this](https://github.com/Netflix/metaflow-ui/blob/master/README.mds)) |
 | Deploy Models from the UI | ❌ | ❌ |
-| Build and push docker images | ✅ | ❌ (possibly with netflix extensions) |
+| Stream logs to UI during DAG run | ❌ | ✅ |
+| Can abort DAG mid run from UI | ??? | ❌ |
+| Build and push docker images for you, per step | ⚠️ (not during local development, only when using remote orchestrator) | ❌ (possibly with netflix extensions) |
 | Easy switch from local to cloud | ✅ | ✅ |
 | Run only certain steps on cloud, others locally | ❌ | ✅ |
+| Favor running steps on prem, burst to cloud | ❌ (SkyPilot supports this, but ZenML's integration does not) | ✅ (with kubernetes) |
 | Run DAGs on Airflow | ✅ | ✅ |
 | Run DAGs on Step Functions + AWS Batch | ❌ | ✅ |
 | No limit to number of collaborators | ??? | ✅ |
+| Thin client | ❌ (55 deps) | ✅ (3 deps) |
+| Simple control plane architecture | ✅ (zenml-server, mysql, simple IaC) | ❌ (postgres, backend, frontend, complex IaC) |
+| supports proper Python packaging structure e.g. `pyproject.toml` for the DAG project | ??? | ❌ |
 
+
+
+> The advantage of Skypilot is that it simply provisions a VM to execute the pipeline on your cloud provider. [Reference](https://docs.zenml.io/user-guide/production-guide/cloud-orchestration#starting-with-a-basic-cloud-stack)
+
+- Does this mean that we CAN NOT have each step be deployed onto its own VM?
+
+> When a pipeline is run with a remote orchestrator a Dockerfile is dynamically generated at runtime.
+
+- So we can't have Docker images built and run one after another when developing locally?
+- Does this also mean that we CAN NOT have a DAG where one step has pandas 1.1.1 and another has pandas 1.1.2? How can we have this if we can't have isolated environments per step *locally*?
--- setup-zenml.sh
+++ setup-zenml.sh
@@ -1,17 +1,26 @@
 #!/bin/bash +e
 
-export AWS_PROFILE=zenml
-export AWS_DEFAULT_REGION=us-west-2
-export AWS_REGION=us-west-2
+# NOTE: some of the `--param` in this script have `=` signs. The commands
+# failed without them for those particular parameters!! Don't ask me why.
+
+# THESE CREDENTIALS ARE NOT REQUIRED! 
+# 1. We log in to ZenML using an admin user.
+# 2. ZenML has access to certain AWS permissions via implicit auth (mounted aws creds/profile or instance role)
+# 3. because we're a ZenML admin, ZenML will attempt to fulfill any requests we make to it using the AWS creds that it has
+
+# export AWS_PROFILE=zenml
+# export AWS_DEFAULT_REGION=us-west-2
+# export AWS_REGION=us-west-2
 
 S3_ARTIFACTS_BUCKET_URI="s3://mlops-club-zeml-hackathon-bucket"
-
+ECR_REPO_URI="491085404175.dkr.ecr.us-west-2.amazonaws.com/zenml-hackathon-repo"
+# /zenml-hackathon-repo
 # zenml integration install aws -y
 
 # From the docs:
 # "This method may constitute a security risk, because it can give users access 
 # to the same cloud resources and services that the ZenML Server itself is configured to access.
-export ZENML_ENABLE_IMPLICIT_AUTH_METHODS=true
+export ZENML_ENABLE_IMPLICIT_AUTH_METHODS="true"
 
 # create a service connector that uses your SSO profile (locally) or the EC2 instance role (on AWS)
 zenml service-connector register "s3-bucket-connector" \
@@ -57,3 +66,42 @@ zenml artifact-store register "s3-store" \
 # ┃         bbee06f52f         │                     │                │               │ thon-bucket                ┃
 # ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━┷━━━━━━━━━━━━━━━━━━━━━┷━━━━━━━━━━━━━━━━┷━━━━━━━━━━━━━━━┷━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
 
+zenml container-registry register "ecr-docker-image-store" \
+    --flavor aws \
+    --connector ecr-connector \
+    --uri="$ECR_REGISTRY_URI"
+
+# Successfully connected container registry `ecr-docker-image-store` to the following resources:
+# ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┯━━━━━━━━━━━━━━━━┯━━━━━━━━━━━━━━━━┯━━━━━━━━━━━━━━━━━━━━┯━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓
+# ┃         CONNECTOR ID         │ CONNECTOR NAME │ CONNECTOR TYPE │ RESOURCE TYPE      │ RESOURCE NAMES              ┃
+# ┠──────────────────────────────┼────────────────┼────────────────┼────────────────────┼─────────────────────────────┨
+# ┃ abfd4c26-fecb-46a4-810c-a87b │ ecr-connector  │ 🔶 aws         │ 🐳 docker-registry │ 491085404175.dkr.ecr.us-wes ┃
+# ┃           52574662           │                │                │                    │ t-2.amazonaws.com           ┃
+# ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┷━━━━━━━━━━━━━━━━┷━━━━━━━━━━━━━━━━┷━━━━━━━━━━━━━━━━━━━━┷━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛
+
+zenml stack register "local-s3-ecr" \
+    --artifact-store s3-store \
+    --container_registry ecr-docker-image-store \
+    --image_builder default \
+    --orchestrator default
+
+# Stack 'local-s3-ecr' successfully registered!
+#               Stack Configuration              
+# ┏━━━━━━━━━━━━━━━━━━━━┯━━━━━━━━━━━━━━━━━━━━━━━━┓
+# ┃ COMPONENT_TYPE     │ COMPONENT_NAME         ┃
+# ┠────────────────────┼────────────────────────┨
+# ┃ CONTAINER_REGISTRY │ ecr-docker-image-store ┃
+# ┠────────────────────┼────────────────────────┨
+# ┃ ORCHESTRATOR       │ default                ┃
+# ┠────────────────────┼────────────────────────┨
+# ┃ ARTIFACT_STORE     │ s3-store               ┃
+# ┗━━━━━━━━━━━━━━━━━━━━┷━━━━━━━━━━━━━━━━━━━━━━━━┛
+#              'local-s3-ecr' stack              
+# No labels are set for this stack.
+# Stack 'local-s3-ecr' with id '10aee893-a692-415a-9f89-ecd99aee236b' is owned by user default.
+# To delete the objects created by this command run, please run in a sequence:
+# 
+# zenml stack delete -y local-s3-ecr                                                                                   
+# Dashboard URL: http://0.0.0.0:8080/stacks
+
+zenml stack set "local-s3-ecr"
